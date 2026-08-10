765a0dfbf933025ee610f777f2b44e5b0de3b203
AkshadaSurya <125860762+AkshadaSurya@users.noreply.github.com>
2026-08-10 22:48:19 +0200

    Fix: Update vite base path configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
+export default defineConfig({ 
     plugins: [react()],
     // Automatically uses the repository name for production builds, and root for local development
-    base: command === 'build' ? '/my-live-portfolio/' : '/',
-  }
-})
+    base: '/my-live-portfolio/',
+ })
 
 
 
