c17be0f969f27fe08cb5b20843c284cee51b048f
AkshadaSurya <125860762+AkshadaSurya@users.noreply.github.com>
2026-08-10 22:37:36 +0200

    config-changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
- 
-export default defineConfig({
-  plugins: [react()],
-  base: '/', 
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Automatically uses the repository name for production builds, and root for local development
+    base: command === 'build' ? '/my-live-portfolio/' : '/',
+  }
 })
+
+
 
 
